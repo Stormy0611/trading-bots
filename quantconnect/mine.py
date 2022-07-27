420d6977d7117e2ad8372570d6340f98f02f39c5
devstar0611 <103535364+devstar0611@users.noreply.github.com>
2022-07-26 19:59:57 -0700

    update mine.py

--- quantconnect/mine.py
+++ quantconnect/mine.py
@@ -6,27 +6,40 @@ from AlgorithmImports import *
 class CryingApricotBison(QCAlgorithm):
 
     def Initialize(self):
+
         self.SetStartDate(2021, 1, 1)  # Set Start Date
         self.SetEndDate(2022, 6, 30)
+
         # self.SetAccountCurrency("BTC")
+
         self.SetCash(100000)  # Set Strategy Cash
+
+        self.SetCash("BTC", 1)
+
+        self.SetBrokerageModel(BrokerageName.GDAX, AccountType.Cash)
         # self.SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Cash)
-        self.SetSecurityInitializer(self.CustomSecurityInitializer)
+
+        # self.SetSecurityInitializer(self.CustomSecurityInitializer)
         # self.SetSecurityInitializer(lambda security: security.SetFeeModel(ConstantFeeModel(0, "USD")))
 
-        self.AddForex("EURUSD", Resolution.Second)  # Add EURUSD 1 second bars
+        self.symbol = self.AddCrypto("BTCUSD", Resolution.Minute)
+        # self.AddForex("EURUSD", Resolution.Second) # Add EURUSD 1 second bars
 
-        # self.__symbol = "AAPL"
-        self.__symbol = self.AddEquity("AAPL", Resolution.Minute).Symbol
-        self.sma_short = self.SMA(self.__symbol, 10)
-        self.sma_long = self.SMA(self.__symbol, 20)
+        # Feed in 100 trading days worth of data before the start date
+        # self.SetWarmUp(100, Resolution.Minute)
 
-    def CustomSecurityInitializer(self, security: Security) -> None:
-        # Disable trading fees
-        security.SetFeeModel(ConstantFeeModel(0, "USD"))
+        self.sma_short = self.SMA(self.symbol, 10)
+        self.sma_long = self.SMA(self.symbol, 20)
+
+    # def CustomSecurityInitializer(self, security: Security) -> None:
+    #     # Disable trading fees
+    #     security.SetFeeModel(ConstantFeeModel(0, "USD"))
+
+    # def OnWarmUpFinished(self) -> None:
+    #     self.Log("Algorithm Ready")
 
     def OnData(self, data: Slice):
         self.PlotIndicator("<chartName>", self.sma_short, self.sma_long)
 
     def OnEndOfAlgorithm(self):
-        self.Debug("Algorithm done")
+        self.Log("Algorithm done")
